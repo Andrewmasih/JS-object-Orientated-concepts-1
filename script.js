f247223e992210ea94829c1758e7adcea27248b9
Andrew Masih <armasih@outlook.com>
2021-05-04 13:00:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*OOP are based on objects instead of the actions and data, it allows to model the real world using objects, object used to build JS Apps that model reality.JS is all about objects, that have data. Example below*/
+
+let car = {
+    owner: "Marty Mcfly",
+    manufacture: "DeLorean",
+    year: 1985,
+    wheels: 0,
+    fuelType: "bananas"
+};
+
+console.log (car);
+
+/*'objects' have 'data' stored in 'key-value pairs',objects are used to mimic things in real life */
+
+/*each object can process data, receieve and send messages. objects are like self-contained pieces of code that can be reused to build apps, they are independent but still part of a importanat mechanism */
+
